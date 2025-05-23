68fcc71e1c2092f4114238161bd4c9b1977c6141
Walter330391 <walter.souza0311@gmail.com>
2025-05-23 19:14:59 -0300

    Update teste_01.py

--- testes/teste_01.py
+++ testes/teste_01.py
@@ -1,1 +1,17 @@
 teste01
+import unittest
+
+class TestWebsiteAccessibility(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+
+    def test_website_accessibility(self):
+        self.driver.get('https://www.example.com')
+        self.assertIn('Example Domain', self.driver.title)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
